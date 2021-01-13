b1bc17db7a2a67e0752082e380d6dba0a7bd75c8
XDwightsBeetsX <55027279+XDwightsBeetsX@users.noreply.github.com>
2021-01-12 20:22:42 -0600

    js listens for "enter" on input

--- brackets.js
+++ brackets.js
@@ -1,6 +1,28 @@
+/*===========================*/
+/*===== On Window Load ======*/
+/*===========================*/
+window.onload = function() {
+    listenForEntry();
+}
+
+function listenForEntry() {
+    var input = document.getElementById('entry');
+    input.addEventListener('keyup', function(event) {
+        if (event.code === 'Enter') {
+            event.preventDefault();
+            var submitButton = document.getElementById('btn-entry-submit');
+            submitButton.click();
+        }
+    });
+}
 
 
+/*===========================*/
+/*========= Dynamic =========*/
+/*===========================*/
 function newEntry() {
-    var entry = document.getElementById("entry").value;
-    alert(entry);
+    var input = document.getElementById("entry");
+    var entry = input.value;
+    input.value = '';
+    alert("Added " + entry + "!");
 }
