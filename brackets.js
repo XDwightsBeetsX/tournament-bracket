5b044003a0e55c589a34dc6c019ec8126942b8dd
XDwightsBeetsX <55027279+XDwightsBeetsX@users.noreply.github.com>
2021-01-12 20:45:51 -0600

    entries names added to left side and input is cleared

--- brackets.js
+++ brackets.js
@@ -1,10 +1,21 @@
 /*===========================*/
+/*======== Gloabals =========*/
+/*===========================*/
+var Entries = [];
+
+
+/*===========================*/
 /*===== On Window Load ======*/
 /*===========================*/
 window.onload = function() {
     listenForEntry();
+    Entries = [];
 }
 
+
+/*===========================*/
+/*===== Event Listeners =====*/
+/*===========================*/
 function listenForEntry() {
     var input = document.getElementById('entry');
     input.addEventListener('keyup', function(event) {
@@ -22,7 +33,23 @@ function listenForEntry() {
 /*===========================*/
 function newEntry() {
     var input = document.getElementById("entry");
-    var entry = input.value;
+    var newEntry = new Entry(input.value);
     input.value = '';
-    alert("Added " + entry + "!");
+    alert("Added " + newEntry.Name + "!");
+    addEntryToList(newEntry);
+}
+
+function addEntryToList(newEntry) {
+    Entries.push(newEntry);
+    var newEntryDiv = document.createElement('div');
+    newEntryDiv.innerHTML = newEntry.Name;
+    document.getElementById("entryList").appendChild(newEntryDiv);
+}
+
+
+class Entry {
+    constructor(name, seed=null) {
+        this.Name = name;
+        this.Seed = seed;
+    }
 }
